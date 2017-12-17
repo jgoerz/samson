e224391e10d7a1e5658e23e0a577f43bcf98bf6c
Jesse Goerz <jesse.goerz@plansource.com>
2017-12-17 16:17:09 -0500

    Modify seeds to load custom data.
    Loads custom data from db/seeds.custom.yml file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,112 @@
 # frozen_string_literal: true
-prod_env = Environment.create!(
-  name: 'Production',
-  production: true
-)
-Environment.create!(name: 'Staging')
-Environment.create!(name: 'Master')
 
-prod = DeployGroup.create!(
-  name: 'Production',
-  environment: prod_env
-)
+if ENV['SETUP_USE_CUSTOM'].nil?
 
-project = Project.create!(
-  name: "Example-project",
-  repository_url: "https://github.com/samson-test-org/example-project.git"
-)
+  prod_env = Environment.create!(
+    name: 'Production',
+    production: true
+  )
+  Environment.create!(name: 'Staging')
+  Environment.create!(name: 'Master')
 
-project.stages.create!(
-  name: "Production",
-  deploy_groups: [prod]
-)
+  prod = DeployGroup.create!(
+    name: 'Production',
+    environment: prod_env
+  )
 
-user = User.create!(
-  name: "Mr. Seed",
-  email: "seed@example.com",
-  external_id: "123"
-)
+  project = Project.create!(
+    name: "Example-project",
+    repository_url: "https://github.com/samson-test-org/example-project.git"
+  )
 
-project.releases.create!(
-  commit: "1234" * 10,
-  author: user
-)
+  project.stages.create!(
+    name: "Production",
+    deploy_groups: [prod]
+  )
+
+  user = User.create!(
+    name: "Mr. Seed",
+    email: "seed@example.com",
+    external_id: "123"
+  )
+
+  project.releases.create!(
+    commit: "1234" * 10,
+    author: user
+  )
+
+else
+
+  puts "Seeding with custom data"
+
+  # Sample data that would load the default above. (it's not quite identical)
+  # ---
+  # projects:
+  #   - name: "Production"
+  #     repository_url: "https://github.com/samson-test-org/example-project.git"
+  #     stages:
+  #       - name: "Production"
+  #         production: true
+  # users:
+  #   - name: "Mr. Seed"
+  #     email: "seed@example.com"
+  #     external_id: "123"
+
+  data = YAML.load(File.read('db/seeds.custom.yml'))
+
+  # Load projects, and their stages and commands.
+  if data.has_key?('projects')
+    data['projects'].each do |project|
+      p_obj = Project.create!(
+        name: project['name'],
+        repository_url: project['repository_url']
+      )
+
+      if project.has_key?('stages')
+        project['stages'].each do |stage|
+          p_obj.stages.create!(
+            name: stage['name'],
+            production: stage['production'],
+            cancel_queued_deploys: stage['cancel_queued_deploys']
+          )
+        end
+      end
+
+      # per project commands
+      if project.has_key?('commands')
+        project['commands'].each do |command|
+          p_obj.commands.create!(
+            command: command['command']
+          )
+        end
+      end
+    end
+  end
+
+  # external_id follows this format:
+  # AUTH_MODULE-IDENTIFIER
+  # example gitlab: gitlab-123
+  # example ldap: ldap-CN=Mr. Seed,OU=Employees,...
+  # role_id: (see app/models/role.rb for appropriate values)
+  if data.has_key?('users')
+    data['users'].each do |user|
+      User.create!(
+        time_format: user['time_format'],
+        role_id: user['role_id'],
+        name: user['name'],
+        email: user['email'],
+        external_id: user['external_id']
+      )
+    end
+  end
+
+  # Global Commands
+  if data.has_key?('commands')
+    data['commands'].each do |command|
+      Command.create!(
+          command: command['command']
+      )
+    end
+  end
+
+end
